5640febae2c8b2ca3e3d77ad937f77e8d8d484b5
楊勝傑 <shengjie@iii.edu.com.tw>
2017-06-03 16:43:45 +0800

    hfahfoadhfohf

--- LabWebServlet/src/main/java/controller/TestServlet.java
+++ LabWebServlet/src/main/java/controller/TestServlet.java
@@ -17,7 +17,7 @@ public class TestServlet extends HttpServlet {
 
 		response.setContentType("text/html; charset=UTF-8"); 
 		PrintWriter out = response.getWriter();
-		out.println("<h1>碼全德</h1>");
+		out.println("<h1>楊勝傑</h1>");
 
 		out.close();
 	}
